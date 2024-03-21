c65d7290f29201acff4fd411a6e1305544f95559
grsendha <grsendha11@gmail.com>
2024-03-21 15:26:09 +0530

    added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
+const { sendBasicEmail } = require("./services/email-service");
 
 const setupServer = () => {
   const app = express();
-  // app.use(bodyParser.urlencoded({ extended: true }));
-  // app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
   app.listen(PORT, () => {
     console.log("server started at ", PORT);
+    const msgBody = `
+    This is First
+    This is second
+    This is third
+    `;
+    sendBasicEmail(
+      "support@gyan.com",
+      "ranjangyan7002@gmail.com",
+      "This is a demo",
+      msgBody
+    );
   });
 };
 
