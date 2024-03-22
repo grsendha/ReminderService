2c471249b48f39ec0ff60d09e4d60e6acd0bc29d
grsendha <grsendha11@gmail.com>
2024-03-22 23:57:55 +0530

    added rabbitmq config

--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { PORT } = require("./config/serverConfig");
-const { sendBasicEmail } = require("./services/email-service");
+const TicketController = require("./controllers/ticket-controller");
 const jobs = require("./utils/job");
+const { sendBasicEmail } = require("./services/email-service");
+const { PORT } = require("./config/serverConfig");
+// const { createChannel } = require("./utils/messageQueue");
 
-const setupServer = () => {
+const setupServer = async () => {
   const app = express();
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
-  const TicketController = require("./controllers/ticket-controller");
+
+  // const channel = await createChannel();
 
   app.listen(PORT, () => {
     console.log("server started at ", PORT);
     app.post("/api/v1/tickets", TicketController.create);
     jobs();
-    // const msgBody = `
-    // This is First
-    // This is second
-    // This is third
-    // `;
-    // sendBasicEmail(
-    //   "support@gyan.com",
-    //   "ranjangyan7002@gmail.com",
-    //   "This is a demo",
-    //   msgBody
-    // );
   });
 };
 
